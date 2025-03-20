4c17ce99ff92d7b2c0452abc0ac13ba9ec57aea5
Yonas <yonasseyoum24@gmail.com>
2025-03-20 21:26:34 +0300

    update routes to dashboard for the time being

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -143,12 +143,14 @@ export default function SignupPage() {
               </label>
             </div>
             <div>
-              <button
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              >
-                {createAccount}
-              </button>
+              <Link href={"/dashboard"}>
+                <button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  {createAccount}
+                </button>
+              </Link>
             </div>
           </form>
           <div className="mt-6">
