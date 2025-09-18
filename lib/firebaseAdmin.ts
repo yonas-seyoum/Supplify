3cc4b4f92ff4a2f3f2940edfc670a2ff962cfefe
Yonas <yonasseyoum24@gmail.com>
2025-09-18 14:36:04 +0300

    add firebase admin file

---
+++ lib/firebaseAdmin.ts
@@ -0,0 +1,14 @@
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+}
+
+export const adminAuth = admin.auth();
+export const adminDB = admin.firestore();
